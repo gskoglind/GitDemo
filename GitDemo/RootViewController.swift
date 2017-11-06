f65ce0b9c20186aa65fbc9f8b32884b23ed2bd83
Gary Skoglind <gary.skoglind@gmail.com>
2017-11-06 10:49:20 -0500

    Initial Commit

---
+++ GitDemo/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  GitDemo
+//
+//  Created by Gary on 11/6/17.
+//  Copyright © 2017 Gary. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
