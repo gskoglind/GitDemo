ee2549936115a1b85e82ead083b1a560a2603cbe
Akiva Levitas <akivalevitas@Akivas-MacBook-Air.local>
2017-11-06 11:42:45 -0500

    update label on storyboard

--- GitDemo/RootViewController.swift
+++ GitDemo/RootViewController.swift
@@ -14,7 +14,7 @@ class RootViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       gitDemoLabel.text = "I changed this"
     }
 
 }
